b3488c5f00d9010aa4792ae70fca30e3815120ad
David Jegat <david.jegat@gmail.com>
2022-05-03 12:08:57 +0200

    Ajout de la correction du composant Hello

---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Hello({ name }) {
+  return <h1>Bonjour {name}</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-/**
- * Créer un composant Hello qui affiche
- * une h1 avec "Hello {name} !"
- * 
- * Ce composant doit être dans son propre fichier
- * et exporté par défaut
- * 
- * Importé le composant dans index.js et l'afficher
- * dans le render
- */
+import Hello from './Hello'
 
-
-render(/* Ici, afficher le composant Hello */, div)
+render(<Hello />, document.querySelector('#root'))
