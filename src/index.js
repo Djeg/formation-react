24727ab64d3de85df0961ebc84f1e78c91f6a35b
David Jegat <david.jegat@gmail.com>
2022-05-03 11:19:25 +0200

    Ajout d'un composant Hello

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const notes = [12, 15, 8, 16, 9]
+function Hello() {
+  return <h1>Bonjour tout le monde !</h1>
+}
 
 const html = (
   <div>
-    <h1>Vos notes</h1>
-    <ul>
-      {notes.map((note, index) => (
-        <li key={`note-${index}`}>{note} / 20</li>
-      ))}
-    </ul>
+    <Hello />
   </div>
 )
 
