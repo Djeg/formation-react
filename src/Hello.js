32c904af1b685ecb9b3008cb8f13be56de772460
David Jegat <david.jegat@gmail.com>
2022-05-03 13:42:10 +0200

    Ajout des exos design

--- src/Hello.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Hello({ name }) {
-  return <h1>Bonjour {name}</h1>
-}
---
+++ src/exos/design5/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+/**
+ * Contient tout les composants (ainsi
+ * que le css pour le design 5)
+ *
+ * @ref https://www.figma.com/file/uqUjILdONGoXCEImw1iYqv/Untitled?node-id=0%3A1
+ */
+export default function App() {}
