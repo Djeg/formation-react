57a8b35279fbbead8ff44a99838771b7d6934946
David Jegat <david.jegat@gmail.com>
2022-05-05 12:26:49 +0200

    Ajout d'un exo conteur

---
+++ src/exos/conteur/App.js
@@ -0,0 +1,21 @@
+/**
+ * Créer un application avec un conteur.
+ *
+ * L'application affiche un counteur (0 au départ),
+ * 3 boutons (un bouton plus, un bouton moins, un bouton
+ * réinitialiser)
+ * Ainsi qu'un input type="number" avec la valeur 1
+ *
+ * -> Lors du clique sur plus, augmenter le compteur de
+ *    la valeur contenu dans l'input number
+ * -> Lors du clique sur moins, diminuer le compteur de de
+ *    la valeur contenu dans l'input number
+ * -> Lors du clique sur réinitialiser, remettre le conteur à 0
+ *
+ * !ATTENTION! Le conteur ne doit pas descendre en dessous de 0 !
+ */
+import React from 'react'
+
+export default function App() {
+  return <></>
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// On importe la librairie React qui permet
+// d'écrire avec la syntax JSX
 import React from 'react'
+// On importe la fonction render depuis la librarie
+// react-dom. Cette fonction permet d'afficher
+// un composant dans la page
 import { render } from 'react-dom'
-import App from './exos/todo/App'
+// On importe la fonction App depuis le fichier
+// exos/todo/App.js. Cette fonction est un composant
+import App from './exos/conteur/App'
 import './index.css'
 
 render(<App />, document.querySelector('#root'))
