8e89177cfc02c05d06ac6e7e52de732554f83598
David Jegat <david.jegat@gmail.com>
2022-05-05 16:29:21 +0200

    Correction du carnet de note

--- src/exos/carnet-notes/App.js
+++ src/exos/carnet-notes/App.js
@@ -7,7 +7,7 @@
  * - Afficher dans une balise <ul> / <li> toutes les notes rentrées
  *
  * Bonus : Ajouter un bouton "supprimer" sur une note dans la balise <li>
- * Bonus : Ajouter la possibilité de modier une note en éditant un input
+ * Bonus : Ajouter la possibilité de modifer une note en éditant un input
  *         dans la balise <li>
  *
  * Lorsque je rentre un chiffre dans l'input, que je clique sur ajouter
@@ -21,8 +21,98 @@
  *         sur le bouton supprimer
  * Bonus : Pouvoir éditer un notes
  */
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
-  return <></>
+  const [newNote, setNewNote] = useState(0)
+  const [notes, setNotes] = useState([])
+
+  const changeNewNote = ev => {
+    let note = parseInt(ev.target.value)
+
+    if (note === NaN) {
+      return setNewNote(0)
+    }
+
+    if (note < 0) {
+      return setNewNote(0)
+    }
+
+    if (note > 20) {
+      return setNewNote(20)
+    }
+
+    setNewNote(note)
+  }
+
+  const addNewNote = () => {
+    setNotes([newNote, ...notes])
+    setNewNote(0)
+  }
+
+  /*const deleteNote = index => {
+    const newNotes = []
+
+    for (let i in notes) {
+      const note = notes[i]
+
+      if (parseInt(i) === index) {
+        continue
+      }
+
+      newNotes.push(note)
+    }
+
+    setNotes(newNotes)
+  }*/
+  const deleteNote = index => setNotes(notes.filter((note, i) => i !== index))
+
+  /*const changeNote = (index, ev) => {
+    const newNote = parseInt(ev.target.value)
+    const newNotes = []
+
+    for (let i in notes) {
+      let note = notes[i]
+
+      if (parseInt(i) === index) {
+        newNotes.push(newNote)
+        continue
+      }
+
+      newNotes.push(note)
+    }
+
+    setNotes(newNotes)
+  }*/
+  const changeNote = (index, ev) =>
+    setNotes(
+      notes.map((note, i) => (i === index ? parseInt(ev.target.value) : note)),
+    )
+
+  return (
+    <>
+      <h1>Carnet de notes</h1>
+      <input
+        type="number"
+        placeholder="Votre note ..."
+        value={newNote}
+        onChange={changeNewNote}
+      />
+      <button onClick={addNewNote}>Ajouter</button>
+      <ul>
+        {notes.map((note, index) => {
+          return (
+            <li key={`note-${index}`}>
+              <input
+                type="number"
+                value={note}
+                onChange={ev => changeNote(index, ev)}
+              />
+              <button onClick={() => deleteNote(index)}>Supprimer</button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
 }
