ffdc66361721cac85474c4ccf5bf884a309f0fc2
David Jegat <david.jegat@gmail.com>
2022-05-05 16:49:44 +0200

    Correction de l'app Todo

--- src/exos/todo/App.js
+++ src/exos/todo/App.js
@@ -1,5 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  AddTodoButton,
+  InputText,
+  Todo,
+  TodoInput,
+  TodoLabel,
+  RemoveTodoButton,
+} from './styled'
 
 export default function App() {
-  return <></>
+  const [newLabel, setNewLabel] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  const changeNewLabel = ev => setNewLabel(ev.target.value)
+  const addNewTodo = () => {
+    setTodoList([{ label: newLabel, done: false }, ...todoList])
+
+    setNewLabel('')
+  }
+  const toggleTodo = index => {
+    setTodoList(
+      todoList.map((todo, i) => {
+        if (i === index) {
+          return { ...todo, done: !todo.done }
+        }
+
+        return todo
+      }),
+    )
+  }
+  const removeTodo = index =>
+    setTodoList(todoList.filter((todo, i) => i !== index))
+
+  return (
+    <>
+      <TodoInput>
+        <InputText value={newLabel} onChange={changeNewLabel} />
+        <AddTodoButton onClick={addNewTodo}>
+          <i class="fa-solid fa-circle-plus"></i>
+        </AddTodoButton>
+      </TodoInput>
+
+      {todoList.map((todo, index) => (
+        <Todo done={todo.done} key={`todo-${index}`}>
+          <TodoLabel onClick={() => toggleTodo(index)}>{todo.label}</TodoLabel>
+          <RemoveTodoButton onClick={() => removeTodo(index)}>
+            <i class="fa-solid fa-trash"></i>
+          </RemoveTodoButton>
+        </Todo>
+      ))}
+    </>
+  )
 }
