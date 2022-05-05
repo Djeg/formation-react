3f0e35bd18a1127504ed6847fd4eef10802f7e37
David Jegat <david.jegat@gmail.com>
2022-05-05 11:12:29 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { addTodo, newTodo, removeTodo, toggleTodo } from './exos/algo'
 import App from './exos/todo/App'
 import './index.css'
-
-const todoList = [
-  newTodo('Ranger la chambre'),
-  newTodo('Faire le ménage'),
-  newTodo('Faire la cuisine'),
-]
-
-console.log(todoList)
-
-console.log(toggleTodo(todoList, 1))
 
 render(<App />, document.querySelector('#root'))
