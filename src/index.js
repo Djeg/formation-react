e0fc1656e8c18621800ca22296a51a587ce08387
David Jegat <david.jegat@gmail.com>
2022-05-06 09:27:21 +0200

    Ajout de createRoot

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import React from 'react'
 // On importe la fonction render depuis la librarie
 // react-dom. Cette fonction permet d'afficher
 // un composant dans la page
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 // On importe la fonction App depuis le fichier
 // exos/todo/App.js. Cette fonction est un composant
 import './index.css'
 import App from './todo-app/App'
 
-render(<App />, document.querySelector('#root'))
+const root = createRoot(document.querySelector('#root'))
+
+root.render(<App />)
