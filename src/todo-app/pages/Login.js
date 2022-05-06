3eccc885fd4b5e3794e4905d11606953da1a92ce
David Jegat <david.jegat@gmail.com>
2022-05-06 13:40:29 +0200

    Ajout du login

--- src/todo-app/pages/Login.js
+++ src/todo-app/pages/Login.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
-  return <h1>Connexion</h1>
+  return (
+    <>
+      <h1>Connexion</h1>
+      <form>
+        <input type="email" name="email" placeholder="Votre email" />
+        <input type="password" name="password" placeholder="Mot de passe" />
+        <button type="submit">Connexion</button>
+        <Link to="/inscription">
+          Vous n'avez pas de compte ? Inscrivez-vous
+        </Link>
+      </form>
+    </>
+  )
 }
