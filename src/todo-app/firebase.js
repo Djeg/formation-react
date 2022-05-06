ad0cb14eb8fba67504e5a309f6edf6b083deac66
David Jegat <david.jegat@gmail.com>
2022-05-06 14:34:31 +0200

    Ajout de la db firestore

--- src/todo-app/firebase.js
+++ src/todo-app/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +23,5 @@ export const app = initializeApp(firebaseConfig)
 export const analytics = getAnalytics(app)
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app)
+// INitialize the firestore database and get a reference
+export const db = getFirestore(app)
