48b76b77a72561e9885eae037f2585f15dc90242
David Jegat <david.jegat@gmail.com>
2022-05-06 09:52:37 +0200

    Ajout de l'inscription

--- src/todo-app/pages/CreateAccount.js
+++ src/todo-app/pages/CreateAccount.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export default function CreateAccount() {
-  return <h1>Inscription</h1>
+  return (
+    <>
+      <h1>Inscription</h1>
+      <form>
+        <input type="email" name="email" placeholder="Votre email ..." />
+        <input
+          type="password"
+          name="password"
+          placeholder="Votre mot de passe ..."
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Répéter votre mot de passe ..."
+        />
+        <button type="submit">S'inscrire</button>
+      </form>
+    </>
+  )
 }
