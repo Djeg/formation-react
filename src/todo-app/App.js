7d5f0f952188ffb396bfdb6f2362e01d9a7684b0
David Jegat <david.jegat@gmail.com>
2022-05-06 09:45:49 +0200

    Ajout des 3 pages dans l'App

--- src/todo-app/App.js
+++ src/todo-app/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import TodoApp from './pages/TodoApp'
+import Login from './pages/Login'
+import CreateAccount from './pages/CreateAccount'
 
 export default function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoApp />} />
+        <Route path="/connexion" element={<Login />} />
+        <Route path="/inscription" element={<CreateAccount />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
