d5a4d234e9e46bb240f83575ba771efab4e23cd1
David Jegat <david.jegat@gmail.com>
2022-05-17 14:55:56 +0200

    Ajout de App

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+/**
+ * Ce fichier contient le composant principal
+ * qui lui même contient toute l'application.
+ */
+export default function App() {
+  return <h1>TodoList</h1>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,69 +3,9 @@ import React from 'react'
 // On importe la librairie react-dom (elle permet
 // d'afficher react sur la page)
 import { createRoot } from 'react-dom/client'
-
-//const notes: Array<number> = [12, 13, 8, 14, 20]
-//const age: number = 18
-//
-//// On créé un élément react (JSX)
-//const element: JSX.Element = (
-//  <div>
-//    <h1>Vos notes</h1>
-//    {age >= 18 ? (
-//      <div>
-//        <p>Vous êtes majeur</p>
-//      </div>
-//    ) : (
-//      <div>
-//        <p>Vous êtes mineur</p>
-//      </div>
-//    )}
-//    <ul>
-//      {notes.map((note, index) => (
-//        <li key={`note-${index}`}>{note}/20</li>
-//      ))}
-//    </ul>
-//  </div>
-//)
-
-type HelloProps = {
-  name: string
-  age?: string
-}
-
-function Hello({ name, age = '??' }: HelloProps): JSX.Element {
-  return (
-    <p className="test">
-      Hello {name}, vous avez {age} ans :)
-    </p>
-  )
-}
-
-type BigTextProps = {
-  children: React.ReactNode
-}
-
-function BigText({ children }: BigTextProps): JSX.Element {
-  return (
-    <div className="gros-text2">
-      <h1>{children}</h1>
-    </div>
-  )
-}
-
-const element: JSX.Element = (
-  <div className="test">
-    <BigText>
-      Coucou !!
-      <br />
-      <span>dkfhsdlkfhsdf</span>
-    </BigText>
-    <Hello name="Rose" />
-    <Hello name="Jean" age="35" />
-    <Hello name="Jack" age="21" />
-    <Hello name="Jane" age="40" />
-  </div>
-)
+// On importe le composant App (le composant qui contiendra
+// l'intégralité de notre application)
+import App from './App'
 
 // On récupére la balise div#root de notre
 // page HTML
@@ -77,6 +17,6 @@ if (!root) {
   throw new Error('Oups ... il manque la balise root dans votre index.html')
 }
 
-// J'affiche l'élément à l'écran dans la balise
+// J'affiche le composant App à l'écran dans la balise
 // div#root
-createRoot(root).render(element)
+createRoot(root).render(<App />)
