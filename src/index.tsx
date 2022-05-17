f7e9fca36110707437710fc9f473c6e2cf7fc0a7
David Jegat <david.jegat@gmail.com>
2022-05-17 12:02:03 +0200

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// On importe la librairie react
+import React from 'react'
+// On importe la librairie react-dom (elle permet
+// d'afficher react sur la page)
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const notes: Array<number> = [12, 13, 8, 14, 20]
+//const age: number = 18
+//
+//// On créé un élément react (JSX)
+//const element: JSX.Element = (
+//  <div>
+//    <h1>Vos notes</h1>
+//    {age >= 18 ? (
+//      <div>
+//        <p>Vous êtes majeur</p>
+//      </div>
+//    ) : (
+//      <div>
+//        <p>Vous êtes mineur</p>
+//      </div>
+//    )}
+//    <ul>
+//      {notes.map((note, index) => (
+//        <li key={`note-${index}`}>{note}/20</li>
+//      ))}
+//    </ul>
+//  </div>
+//)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+type HelloProps = {
+  name: string
+  age?: string
+}
+
+function Hello({ name, age = '??' }: HelloProps): JSX.Element {
+  return (
+    <p>
+      Hello {name}, vous avez {age} ans :)
+    </p>
+  )
+}
+
+const element: JSX.Element = (
+  <div>
+    <Hello name="Rose" />
+    <Hello name="Jean" age="35" />
+    <Hello name="Jack" age="21" />
+    <Hello name="Jane" age="40" />
+  </div>
+)
+
+// On récupére la balise div#root de notre
+// page HTML
+const root = document.querySelector('#root')
+
+// Si il n'y a pas de balise root
+if (!root) {
+  // On léve un erreur
+  throw new Error('Oups ... il manque la balise root dans votre index.html')
+}
+
+// J'affiche l'élément à l'écran dans la balise
+// div#root
+createRoot(root).render(element)
