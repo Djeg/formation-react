0e45173757ea78250b87748c3537691cf9588866
David Jegat <david.jegat@gmail.com>
2022-05-19 14:24:27 +0200

    Ajout des états

--- src/subscription/Subscription.tsx
+++ src/subscription/Subscription.tsx
@@ -1,18 +1,42 @@
 import * as UI from '../shared/ui'
-import { AppContainer } from '../shared/ui'
+import { useState } from 'react'
 
 export default function Subscription() {
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+
+  const onChange =
+    (setter: (v: string) => void) =>
+    (e: React.SyntheticEvent<HTMLInputElement>) =>
+      setter(e.currentTarget.value)
+
   return (
     <UI.AppContainer as="form">
       <h1>Inscription</h1>
       <UI.InputContainer>
-        <UI.Input name="username" placeholder="Nom d'utilisateur" />
+        <UI.Input
+          name="username"
+          placeholder="Nom d'utilisateur"
+          onChange={onChange(setUsername)}
+          value={username}
+        />
       </UI.InputContainer>
       <UI.InputContainer>
-        <UI.Input name="email" placeholder="Email" />
+        <UI.Input
+          name="email"
+          placeholder="Email"
+          onChange={onChange(setEmail)}
+          value={email}
+        />
       </UI.InputContainer>
       <UI.InputContainer>
-        <UI.Input name="password" placeholder="Mot de passe" />
+        <UI.Input
+          name="password"
+          placeholder="Mot de passe"
+          onChange={onChange(setPassword)}
+          value={password}
+        />
       </UI.InputContainer>
       <button type="submit">S'inscire</button>
     </UI.AppContainer>
