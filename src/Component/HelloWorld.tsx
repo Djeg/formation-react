6158ae98557766447d99c3e8058a1285bcab75df
Djeg <david.jegat@gmail.com>
2022-12-14 15:19:56 +0100

    Ajout d'un exemple pour les composants

---
+++ src/Component/HelloWorld.tsx
@@ -0,0 +1,7 @@
+/**
+ * Contient le composant Hello World utilisable comme une
+ * balise HTML
+ */
+export default function HelloWorld() {
+  return <h1>Hello World !</h1>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import HelloWorld from './Component/HelloWorld'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
 )
