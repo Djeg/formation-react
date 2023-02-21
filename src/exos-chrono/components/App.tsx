203acd98f0e665445dc49e6481edc3e365324cc4
David Jegat <david.jegat@gmail.com>
2023-02-21 09:59:57 +0100

    Fin de la correction du chrono

--- src/exos-chrono/components/App.tsx
+++ src/exos-chrono/components/App.tsx
@@ -7,13 +7,39 @@ import { displayTime } from '../../lib/time'
 export default function App() {
   // Création des états (seconds, minutes, isRunning)
   const [time, setTime] = useState(0)
-  const [milleseconds, setMilliseconds] = useState(0)
+  const [centiseconds, setCentiseconds] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
+  const [timeMemory, setTimeMemory] = useState<string[]>([])
 
   // Actions permettant d'interargir avec l'interface
   const toggleChrono = () => setIsRunning(!isRunning)
+
+  const incrementTime = () => setTime(t => t + 1)
+
+  const incrementCentiseconds = (actualTime: number) =>
+    setCentiseconds(ms => actualTime % 100)
+
+  const incrementSeconds = (actualCentiseconds: number) =>
+    setSeconds(sec => (actualCentiseconds >= 99 ? (sec + 1) % 60 : sec))
+
+  const incrementMinutes = (actualSeconds: number) =>
+    setMinutes(min => (actualSeconds === 0 && isRunning ? min + 1 : min))
+
+  const resetTime = () => {
+    setTimeMemory([
+      ...timeMemory,
+      `${displayTime(minutes)}:${displayTime(seconds)}:${displayTime(
+        centiseconds,
+      )}`,
+    ])
+    setIsRunning(false)
+    setTime(0)
+    setCentiseconds(0)
+    setSeconds(0)
+    setMinutes(0)
+  }
 
   // Effets du chronomètres
   useEffect(() => {
@@ -25,34 +51,28 @@ export default function App() {
 
     // Déclarion d'un timer : Une fonction
     // qui vas toutes les milliseconds
-    const timer = setInterval(() => {
-      setTime(time => time + 1)
-    }, 10)
+    const timer = setInterval(incrementTime, 10)
 
     // On retourne un effet d'arret
     return () => {
       clearInterval(timer)
-      setIsRunning(false)
+      toggleChrono()
     }
   }, [isRunning])
 
   // Effet permettant d'afficher les milleseconds
   useEffect(() => {
-    setMilliseconds(ms => time % 100)
+    incrementCentiseconds(time)
   }, [time])
 
   // Effet permettant d'afficher les seconds
   useEffect(() => {
-    if (milleseconds >= 99) {
-      setSeconds(sec => (sec + 1) % 60)
-    }
-  }, [milleseconds])
+    incrementSeconds(centiseconds)
+  }, [centiseconds])
 
   // Effet permettant d'afficher les minutes
   useEffect(() => {
-    if (seconds === 0 && isRunning) {
-      setMinutes(min => min + 1)
-    }
+    incrementMinutes(seconds)
   }, [seconds])
 
   return (
@@ -60,10 +80,17 @@ export default function App() {
       <h1>Super Chrono</h1>
       <p>
         {displayTime(minutes)}:{displayTime(seconds)}:
-        {displayTime(milleseconds)}
+        {displayTime(centiseconds)}
       </p>
       <button onClick={toggleChrono}>{isRunning ? 'Pause' : 'Play'}</button>
-      <button>Reset</button>
+      <button onClick={resetTime}>Reset</button>
+      <ul>
+        {timeMemory.map((mem, index) => (
+          <li key={index}>
+            Reset n°{index + 1} : {mem}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
